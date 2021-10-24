f23634b9aa71920c6c3e585abd2b24fcda45a40b
lelderbe <fvdc@ya.ru>
2021-10-24 20:54:22 +0300

    increase font, refactoring, add comments

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
 
-// querySelector, jQuery style
-const $ = function (selector) {
-    return document.querySelector(selector);
-};
-
+/*
+ * Add a new block with id
+ */
 async function add(id) {
-    console.log('ask to add: ', id);
+    // querySelector, jQuery style
+    const $ = function (selector) {
+        return document.querySelector(selector);
+    };
+
+    // console.log('asked to add: ', id);
     const e = document.createElement('div');
     e.className = 'item';
     e.id = id;
-    e.onclick = choosed;
-
-    // Append it and attach the event (via onclick)
+    e.onclick = checkBlock;
     $('.items').appendChild(e);
-    // li.onclick = dynamicEvent;
     count++;
-    // lives = 2;
-    // displayAttempts();
 }
 
 async function shuffle(level) {
-                 // 1  2  3  4  5  6  7  8  9
-    const greens = [1, 1, 1, 2, 2, 3, 3, 3, 3];
     let left = greens[level];
     if (left > count)
-        return ;
+        return ;    // shouldn't happen
     arr = [];
     for (let i = 0; i < count; i++)
         arr.push(0);
@@ -39,7 +40,11 @@ async function shuffle(level) {
 
 function displayAttempts() {
     let result = '';
-    switch (lives) {
+    switch (attempts) {
+        case 4:
+            result += '❤️';
+        case 3:
+            result += '❤️';
         case 2:
             result += '❤️';
         case 1:
@@ -50,69 +55,66 @@ function displayAttempts() {
 
 function displayBlocks() {
     let result = '';
-    console.log('count:', count, ', level:', level, ', greens[level]:', greens[level]);
-    for (let i = 0; i < count - greens[level] - (2 - lives); i++)
+    // console.log('count:', count, ', level:', level, ', greens[level]:', greens[level]);
+    for (let i = 0; i < count - greens[level] - (atmpts[level] - attempts); i++)
         result += '🍎 ';
     for (let i = 0; i < greens[level]; i++)
         result += '🍏 ';
     document.getElementById('level').innerText = result;
-    // document.getElementById('level').innerText = 'Ты на уровне: ' + level;
-    console.log('end of displayBlocks');
 }
 
+/*
+ * Wrong try
+ */
 function wrong(e) {
     e.style.backgroundColor = "red";
     e.onclick = null;
-    lives--;
+    attempts--;
     displayAttempts();
     displayBlocks();
-    console.log('lives:', lives);
-    if (!lives) {
-        endGame();
+    if (!attempts) {
+        gameOver();
     }
 }
 
-function sleep(ms) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms);
-    });
-}
-
+/*
+ * Good try - you got lucky!
+ */
 async function right(e) {
     e.style.backgroundColor = "green";
     await sleep(50);
-    await resetItems();
-    await add(count);
-    // setTimeout(() => resetItems(), 50);
-    // setTimeout(() => add(count), 60);
+    await resetItems(); // setTimeout(() => resetItems(), 50);
+    await add(count); // setTimeout(() => add(count), 60);
     level++;
-    lives = 2;
+    attempts = atmpts[level];
     await shuffle(level);
     displayAttempts();
     displayBlocks();
 }
 
+/*
+ * Reset all blocks: remove background color, restore clicks
+ */
 function resetItems() {
-    let i = 0;
-    while (i < count) {
+    for (let i = 0; i < count; i++) {
         try {
             const e = document.getElementById(i);
             // console.log('reset element:', i, ', elem:', e);
             e.style.removeProperty('background-color');
-            e.onclick = choosed;
-        } catch (e) {
-            // console.log('caught:', e);
-        }
-        i++;
+            e.onclick = checkBlock;
+        } catch (ignore) {}
     }
 }
 
-function endGame() {
+/*
+ * Show all blocks, disable clicks
+ */
+function gameOver() {
     let i = 0;
     while (i < count) {
         try {
             const e = document.getElementById(i);
-            console.log('end game element:', i, ', elem:', e);
+            // console.log('game over element:', i, ', elem:', e);
             if (arr[i]) {
                 e.style.backgroundColor = "green";
             } else {
@@ -126,12 +128,9 @@ function endGame() {
     }
 }
 
-function choosed(e) {
-    // console.log('clicked:', e);
+function checkBlock(e) {
     const id = e.target.id;
-    // console.log('from id:', e.target.id);
     const item = document.getElementById(id);
-    console.log('arr[id]:', arr[id]);
     if (!arr[id]) {
         wrong(item);
     } else {
@@ -143,25 +142,18 @@ async function init() {
     arr[0] = Math.floor(Math.random() * 2);
     arr[1] = !arr[0];
     await add(0);
-    console.log('after add[0]');
     await add(1);
-    console.log('after add[1]');
     displayBlocks();
-    console.log('after displayBlocks');
     displayAttempts();
-    console.log('after displayAttempts');
 }
 
-             // 1  2  3  4  5  6  7  8  9   - count of blocks
-const greens = [1, 1, 1, 2, 2, 3, 3, 3, 3]; // - count of green blocks for level
+             // 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18  - count of blocks
+const greens = [1, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4]; // - count of green blocks for level
+const atmpts = [2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4];
 
 let arr = [];
 let count = 0;
-let lives = 2;
-// console.log('before init arr:', arr, ', count:', count, 'lives:', lives);
+let attempts = 2;
 let level = 1;
 init();
-// document.getElementById('level').innerText = 'Ты на уровне: ' + level;
-// document.getElementById('level').innerText = 'Level: ' + level;
-// $('level').innerText = 'Level: ' + level;
-console.log('after init arr:', arr, ', count:', count, ', level:', level, 'lives:', lives);
+// console.log('arr:', arr, ', count:', count, ', level:', level, 'attempts:', attempts);
